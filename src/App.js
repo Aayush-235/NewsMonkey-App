c52e43d91ac9c8603fec40263f88d3fb9ea4872a
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-06-09 19:17:09 +0530

    Before api call

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+
+export default class App extends Component {
+  c = 'aayush'
+  render() {
+    return (
+      <>
+        <div>
+          <Navbar />
+          <News />
+        </div>
+      </>
+
+    )
+  }
+}
